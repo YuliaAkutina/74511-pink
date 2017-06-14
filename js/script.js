4f05000501f47687205b2c7ebfd3cfd5b3ca3403
Yulia Akutina <akutinayulia@gmail.com>
2017-06-14 19:24:48 +0300

    Упростила хедер: убрала лишние модификаторы открытия/закрытия меню, фон с рукой сделала псевдоэлементом, переписала скрипт открытия/закрытия меню

--- js/script.js
+++ js/script.js
@@ -1,24 +1,13 @@
 var pageHeader = document.querySelector(".page-header");
-var menuTop = document.querySelector(".page-header__menu-top");
-var navMain = document.querySelector(".main-nav");
-var navToggler = document.querySelector(".nav-toggler");
+var navToggler = document.querySelector(".page__header-toggler");
 
 pageHeader.classList.add("page-header--menu-close");
-menuTop.classList.add("page-header__menu-top--close");
-navMain.classList.add("main-nav--close");
-navToggler.classList.add("nav-toggler--close");
 
 navToggler.addEventListener("click", function() {
-  if (navMain.classList.contains("main-nav--close"))
+  if (pageHeader.classList.contains("page-header--menu-close"))
     {
-      navMain.classList.remove("main-nav--close");
       pageHeader.classList.remove("page-header--menu-close");
-      menuTop.classList.remove("page-header__menu-top--close");
-      navToggler.classList.remove("nav-toggler--close");
     } else {
       pageHeader.classList.add("page-header--menu-close");
-      menuTop.classList.add("page-header__menu-top--close");
-      navMain.classList.add("main-nav--close");
-      navToggler.classList.add("nav-toggler--close");
     }
 });
